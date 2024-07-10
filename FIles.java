f811d13eb673020bcfe7c9e5c763424dcd1e196d
Riya Raval <162584138+Cheese101101@users.noreply.github.com>
2024-07-10 23:22:44 +0100

    adding functionality to save to a file

---
+++ FIles.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.File;
+
+
+public class FIles{
+    static StringBuffer fileText = new StringBuffer();
+
+    public static void deleteFile(){
+        File file = new File("storage");
+        file.delete();
+    }
+    public static void readFile(){
+        String str = "";
+        try(BufferedReader fileReader = new BufferedReader(new FileReader("storage"))){
+            while((str = fileReader.readLine()) != null){
+                fileText.append(str).append("\n");
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void rewriteFile(String webName, String type, String name){
+        String oldName = "";
+        String str = "";
+        boolean found = false;
+        try(BufferedReader fileReader = new BufferedReader(new FileReader("storage"))){
+            while((str = fileReader.readLine()) != null){
+                if(str.startsWith(webName)){
+                    found = true;
+                }
+                if(str.startsWith(type) && (found)){
+                    oldName = str.substring(type.length()).trim();
+                    found = false;
+                }
+            }
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        int startIndex = fileText.indexOf(oldName);
+        int endIndex = startIndex + oldName.length();
+        fileText.replace(startIndex, endIndex, name);
+    }
+
+    public static void writeFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("storage"))) {
+            writer.write(fileText.toString());
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args){
+        readFile();
+        rewriteFile("pp", "password: ","awesome8");
+        deleteFile();
+        writeFile();
+    }
+
+}
